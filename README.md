a6b256cf0d6bb63f70d34e192d711075c1fcf925
lukaarma <lukaarma@gmail.com>
2022-07-29 12:06:35 +0200

    Created logger

--- README.md
+++ README.md
@@ -2,5 +2,11 @@
 
 PWM is a simple passwor manager website that encrypts locally yours credentials and syncs them accros all your devices.
 
+## Available enviroment variables
+
+| Variable name  | Available values | Description |
+| -------------- | ---------------- | ----------- |
+| LOG_LEVEL  | debug, verbose, info, warn, error  | Set the console log level, log files are unaffected  |
+
 [lukaarma]: https://github.com/lukaarma
 [Lorenzo Romio]: https://github.com/lorenzoromio
---
+++ src/utils/logger.ts
@@ -0,0 +1,78 @@
+import chalk from 'chalk';
+import winston from 'winston';
+
+const levels = ['debug', 'verbose', 'info', 'warn', 'error'];
+
+export function initLogger(): void {
+
+    // We leave the default info level except if LOG_LEVEL set or if dev enviroment
+    if (process.env.LOG_LEVEL && levels.includes(process.env.LOG_LEVEL)) {
+        winston.level = process.env.LOG_LEVEL;
+    }
+    else if (!process.env.LOG_LEVEL && process.env.NODE_ENV === 'development') {
+        winston.level = 'verbose';
+    }
+
+    // Console transport always present, log based on
+    winston.add(new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.errors({ stack: true }),
+            winston.format.timestamp({ format: 'YYYY-MM-DD hh:mm:ss' }),
+            winston.format.printf(
+                (item: winston.Logform.TransformableInfo) => customLogPrint(item)
+            )
+        )
+    }));
+
+    // File transports based on enviroment:
+    // DEVELOPMENT => Every message logged, wiped every run
+    // PRODUCTION => Errors only, never deleted
+    if (process.env.NODE_ENV === 'development') {
+        winston.add(new winston.transports.File({
+            filename: 'logs/debug.log',
+            level: 'debug',
+            format: winston.format.json(),
+            options: {
+                flags: 'w'
+            }
+        }));
+    }
+    else if (process.env.NODE_ENV === 'production') {
+        winston.add(new winston.transports.File({
+            filename: 'logs/error.log',
+            level: 'error',
+            format: winston.format.json()
+        }));
+    }
+    else {
+        winston.warn('No enviroment detected, no file transport created. Check your NODE_ENV variable!');
+    }
+}
+
+
+function customLogPrint(info: winston.Logform.TransformableInfo): string {
+    switch (info.level) {
+        case 'error': {
+            if (info.fatal) {
+                return `${chalk.red('\n\n[FATAL ERROR]')} ${info.stack ?? info.message ?? 'NO ERROR INFO'}`;
+            }
+
+            return `${chalk.red('\n[ERROR]')} ${info.stack ?? info.message ?? 'NO ERROR INFO'}\n`;
+        }
+        case 'warn': {
+            return `${chalk.yellow('[WARNING]')} ${info.stack ?? info.message ?? 'NO WARNING INFO'}`;
+        }
+        case 'info': {
+            return String(info.message);
+        }
+        case 'verbose': {
+            return chalk.cyan(`[VERBOSE] ${info.message}`);
+        }
+        case 'debug': {
+            return chalk.magenta(`[DEBUG] ${info.message}`);
+        }
+        default: {
+            return chalk.yellow('\n[WARNING] Invalid log level!\n');
+        }
+    }
+}
